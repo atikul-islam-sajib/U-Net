9abddbff6083543fb9f10eabbbd70623c024a60d
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-03-18 23:30:51 +0100

    dataloader is passed in the unittest

--- unittest/unit-test.py
+++ unittest/unit-test.py
@@ -1,31 +1,30 @@
-# import sys
-# import os
-# import unittest
+import sys
+import os
+import unittest
+import torch
 
-# sys.path.append("src/")
+sys.path.append("src/")
 
-# from utils import load_pickle
-# from config import PROCESSED_PATH
-# from dataloader import Loader
-
-
-# class UnitTest(unittest.TestCase):
-#     def setUp(self):
-#         self.dataloader = load_pickle(os.path.join(PROCESSED_PATH, "dataloader.pkl"))
-
-#     def test_quantity_data(self):
-#         self.assertEqual(sum(data.size(0) for data, label in self.dataloader), 780)
+from utils import load_pickle
+from config import PROCESSED_PATH
+from dataloader import Loader
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+class UnitTest(unittest.TestCase):
+    def setUp(self):
+        self.dataloader = load_pickle(os.path.join(PROCESSED_PATH, "dataloader.pkl"))
 
-import joblib
+    def test_quantity_data(self):
+        self.assertEqual(sum(data.size(0) for data, label in self.dataloader), 780)
 
-data = joblib.load(
-    "/Users/shahmuhammadraditrahman/Desktop/U-Net/data/processed/dataloader.pkl"
-)
+    def test_RGB(self):
+        data, label = next(iter(self.dataloader))
+        self.assertEqual(data.size(), torch.Size([32, 3, 256, 256]))
 
-data, label = next(iter(data))
+    def test_GRAY(self):
+        _, label = next(iter(self.dataloader))
+        self.assertEqual(label.size(), torch.Size([32, 1, 256, 256]))
 
-print(data.shape)
+
+if __name__ == "__main__":
+    unittest.main()
