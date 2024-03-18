f042288f0f474ed440859028351de6e98a5fbb42
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-03-18 22:20:48 +0100

    utils file is updated in the src

---
+++ src/utils.py
@@ -0,0 +1,26 @@
+# Import all libraries
+import os
+import yaml
+import zipfile
+import cv2
+import joblib
+import torch
+from PIL import Image
+
+
+def clean(filename):
+    if filename:
+        for file in os.listdir(filename):
+            os.remove(os.path.join(filename, file))
+    else:
+        raise ValueError("File not found".capitalize())
+
+
+def config():
+    with open("./deafult_params.yml", "r") as file:
+        return yaml.safe_load(file)
+
+
+def load_pickle(value=None, filename=None):
+    if value is not None and filename is not None:
+        joblib.dump(value=value, filename=filename)
