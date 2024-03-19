9fbaa1f34005dbf7cca7e3031cacbc39acaecb6c
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-03-19 13:12:40 +0100

    encoder code is passed in the unittest

--- unittest/unit-test.py
+++ unittest/unit-test.py
@@ -8,11 +8,14 @@ sys.path.append("src/")
 from utils import load_pickle
 from config import PROCESSED_PATH
 from dataloader import Loader
+from encoder import Encoder
 
 
 class UnitTest(unittest.TestCase):
     def setUp(self):
         self.dataloader = load_pickle(os.path.join(PROCESSED_PATH, "dataloader.pkl"))
+        self.encoder = Encoder(in_channels=3, out_channels=64)
+        self.noise_samples = torch.randn(64, 3, 256, 256)
 
     def test_quantity_data(self):
         self.assertEqual(sum(data.size(0) for data, label in self.dataloader), 780)
@@ -29,6 +32,11 @@ class UnitTest(unittest.TestCase):
         self.assertEqual(
             sum(data.size(0) + label.size(0) for data, label in self.dataloader),
             780 * 2,
+        )
+
+    def test_encoder_block(self):
+        self.assertEqual(
+            self.encoder(self.noise_samples).shape, torch.Size([64, 64, 256, 256])
         )
 
 
