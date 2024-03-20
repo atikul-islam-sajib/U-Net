ca97ef6f0d1df66517b80b2180cf780f9e6a2140
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-03-20 21:58:47 +0100

    device init is added in the utils file

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@
 import os
 import yaml
 import joblib
+import torch
 import torch.nn as nn
 
 
@@ -36,3 +37,12 @@ def weight_init(m):
     elif classname.find("BatchNorm") != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
+
+
+def device_init(device="mps"):
+    if device == "mps":
+        return torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    elif device == "cuda":
+        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    else:
+        return torch.device("cpu")
